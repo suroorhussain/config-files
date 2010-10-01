4a3ba1f17b1618f382c7fbbffb5bd3f1c1f073fd
Justin Van Winkle <justin@milo.com>
2010-09-30 17:32:36 -0700

    add pg log fixer

---
+++ bin/pg_fixlog.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+
+_line_re = re.compile(r'postgres\[\d+\]: \[\d+\-\d+\](.*)')
+
+
+def test_re():
+    line = ('Sep 29 14:16:34 stub postgres[31489]: [36-3]'
+            '	FROM pluto_dev.merchant_addresses')
+
+    assert _line_re.search(line).group(1) == \
+        '	FROM pluto_dev.merchant_addresses'
+
+
+if __name__ == '__main__':
+    while True:
+        line = sys.stdin.readline()
+        m = _line_re.search(line)
+        if m:
+            print m.group(1)
+        else:
+            print line
