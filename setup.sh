638f3ce38f98b3c9a5b5071cd34d2518d9e84498
Justin Van Winkle <justin@milo.com>
2013-11-22 14:06:56 -0800

    setup script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for f in \
+    .bashrc \
+    bin \
+    .curlrc \
+    .emacs.d \
+    .fonts.conf \
+    .gitconfig \
+    .gtkrc \
+    .gtkrc-2.0 \
+    .hgrc \
+    .profile \
+    .sbclrc \
+    .tidyrc \
+    .xinitrc \
+    .xmodmap \
+    .Xmodmap \
+    .xmonad \
+    .Xresources \
+    .zshrc \; do
+    rm $f;
+    ln -s .config-files/$f ;
+done
