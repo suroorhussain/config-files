55e5aa4fd06f2bd92615cbbda72b5b18a9353da4
Justin Van Winkle <justin@milo.com>
2013-12-07 19:49:57 -0800

    stop adding emacs.d to load-path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/libs")
 (add-to-list 'load-path "~/.emacs.d/color-theme")
 (add-to-list 'load-path "~/.emacs.d/icicles")
 (add-to-list 'load-path "~/.emacs.d/slime")
