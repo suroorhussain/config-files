9507a9476329b04f203bd2a1f85d9c9f955c8080
Justin Van Winkle <justin@milo.com>
2014-01-01 12:18:02 -0800

    upgrade icicles...again and again and again

--- .emacs.d/elpa/icicles-20140101.108/icicles-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "icicles" "20140101.108" "Minibuffer input completion and cycling." 'nil)
---
+++ .emacs.d/elpa/icicles-20140101.1751/icicles-pkg.el
@@ -0,0 +1,1 @@
+(define-package "icicles" "20140101.1751" "Minibuffer input completion and cycling." 'nil)
