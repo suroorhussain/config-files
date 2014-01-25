e4a967587c62c5553b50b9ebdec3235181baf2eb
Justin Van Winkle <justin@milo.com>
2014-01-25 03:31:47 -0800

    libs

--- .emacs.d/conf.d/set-key.el
+++ .emacs.d/conf.d/set-key.el
@@ -3,5 +3,6 @@
 (global-set-key (kbd "M-RET") 'toggle-frame-fullscreen)
 (global-set-key (kbd "C-\\") 'condense-whitespace)
 (global-set-key (kbd "M-c") 'kill-ring-save)
-(global-set-key (kbd "C-=") 'flymake-goto-next-error)
+(global-set-key (kbd "M-n") 'flycheck-next-error)
+(global-set-key (kbd "M-p") 'flycheck-previous-error)
 (global-set-key (kbd "C-o") 'find-file-in-repository)
