18ee2d3f0e49135756f599e84fa01fc0c0d379a6
Justin Van Winkle <justin@milo.com>
2015-01-12 12:56:07 -0800

    upgrade

--- .emacs.d/elpa/clang-format-20141125.1418/clang-format-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; clang-format-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (clang-format-region) "clang-format" "clang-format.el"
-;;;;;;  (21630 4923 871866 820000))
-;;; Generated autoloads from clang-format.el
-
-(autoload 'clang-format-region "clang-format" "\
-Use clang-format to format the code between START and END according to STYLE.
-If called interactively uses the region or the current buffer if there
-is no active region.  If no style is given uses `clang-format-style'.
-
-\(fn START END &optional STYLE)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("clang-format-pkg.el") (21630 4923 889336
-;;;;;;  448000))
-
-;;;***
-
-(provide 'clang-format-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; clang-format-autoloads.el ends here
--- .emacs.d/elpa/clang-format-20141125.1418/clang-format-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clang-format" "20141125.1418" "Format code using clang-format" (quote ((json "1.3"))))
