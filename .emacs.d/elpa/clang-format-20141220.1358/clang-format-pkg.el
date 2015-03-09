4891bb2569e674a0ba0de22e3f324c2a55307b48
Justin Van Winkle <justin@milo.com>
2015-03-09 11:00:35 -0700

    upgrade

--- .emacs.d/elpa/clang-format-20141220.1358/clang-format-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clang-format" "20141220.1358" "Format code using clang-format" '((json "1.3")) :keywords '("tools" "c"))
--- .emacs.d/elpa/py-autopep8-20140908.31/py-autopep8-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "py-autopep8" "20140908.31" "Use autopep8 to beautify a Python buffer" (quote nil))
