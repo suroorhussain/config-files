b7e221d397a978f51d907b6279d755509f227dda
Justin Van Winkle <justin@milo.com>
2018-06-13 19:56:47 -0400

    derp

--- .emacs.d/elpa/dash-20170810.137/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (22956 17398 927070 600000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
