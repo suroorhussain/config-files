a5db4ac7da97c1bceb697877c164823e7da5676a
Justin Van Winkle <justin@milo.com>
2018-09-12 12:54:35 -0400

    upgrade crap

---
+++ .emacs.d/elpa/concurrent-20161229.330/concurrent-autoloads.el
@@ -0,0 +1,15 @@
+;;; concurrent-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("concurrent.el") (23449 17567 805430 445000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; concurrent-autoloads.el ends here
--- .emacs.d/elpa/s-20180406.108/s-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("s.el") (23329 44832 496582 437000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-autoloads.el ends here
